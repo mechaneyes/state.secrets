f5b0b45d2e8794c44987c3f5e0c160aaa4ebd7ab
mechaneyes <ray@mechaneyes.com>
2024-02-09 11:13:19 -0500

    added metadata incl og:image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
+import { Metadata } from "next";
 import Image from "next/image";
 import "./styles/main.scss"
+
+// https://medium.com/@moh.mir36/open-graph-with-next-js-v13-app-directory-22c0049e2087
+//
+export const metadata: Metadata = {
+  metadataBase: new URL("https://state.secrets/"),
+  title: "State Secrets",
+  description: "Watching the Watchers",
+  openGraph: {
+    images: [
+      {
+        url: "https://res.cloudinary.com/dhbk213bt/image/upload/v1706917124/lightwerk/og/og-shadowing.jpg",
+      },
+    ],
+  },
+};
 
 export default function Home() {
   return (
